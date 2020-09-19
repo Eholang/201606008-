c6c986c01d7c3ffa69d582083aa29fe31b84a9d2
Eholang <70881011+Eholang@users.noreply.github.com>
2020-09-20 03:05:09 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,13 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _201606008_이무비
+namespace _201606008_이무비_2주차
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("201606008 이무비");
+            Console.Write("변수1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("변수2");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("연산자");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.Write(n1 + n2);
+                    break;
+                case "-":
+                    Console.Write(n1 - n2);
+                    break;
+                case "*":
+                    Console.Write(n1 * n2);
+                    break;
+                case "/":
+                    Console.Write(1 / n2);
+                    break;
+            }
+
         }
     }
 }
